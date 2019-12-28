b7b0b96afb314d4ea158821b5d40adc84700b31d
yuyake0084 <yuyake0084@gmail.com>
2019-12-29 02:29:18 +0900

    :heavy_plus_sign: Add babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
+const { resolve } = require('path')
+
 module.exports = api => {
   const web = process.env.BABEL_ENV !== 'node'
+  const isProd = process.env.NODE_ENV === 'production'
 
   api.cache(true)
 
@@ -17,6 +20,19 @@ module.exports = api => {
       '@babel/preset-react'
     ],
     plugins: [
+      ...(
+        isProd
+          ? [
+            ['module-resolver', {
+              root: ['./src'],
+              alias: {
+                '@client': resolve(__dirname, './dist/server/client'),
+                '@server': resolve(__dirname, './dist/server/server'),
+              },
+            }]
+          ]
+          : []
+      ),
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
