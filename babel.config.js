f2582026633ab50a15a5770a3df80190ceaf97ea
yuyake0084 <yuyake0084@gmail.com>
2019-12-29 03:27:39 +0900

    :construction: WIP

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,10 @@
-const { resolve } = require('path')
+// const { resolve } = require('path')
 
 module.exports = api => {
   const web = process.env.BABEL_ENV !== 'node'
   const isProd = process.env.NODE_ENV === 'production'
 
   api.cache(true)
-
-  console.log('hoge')
 
   return {
     presets: [
@@ -22,19 +20,19 @@ module.exports = api => {
       '@babel/preset-react'
     ],
     plugins: [
-      ...(
-        isProd
-          ? [
-            ['module-resolver', {
-              root: ['./src'],
-              alias: {
-                '@client': resolve(__dirname, './dist/server/client'),
-                '@server': resolve(__dirname, './dist/server/server'),
-              },
-            }]
-          ]
-          : []
-      ),
+      // ...(
+      //   isProd
+      //     ? [
+      //       ['module-resolver', {
+      //         root: ['./src'],
+      //         alias: {
+      //           '@client': resolve(__dirname, './dist/server/client'),
+      //           '@server': resolve(__dirname, './dist/server/server'),
+      //         },
+      //       }]
+      //     ]
+      //     : []
+      // ),
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
