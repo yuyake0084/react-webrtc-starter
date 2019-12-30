7d3fcbffaf239007450f81940ca939e82681fed6
yuyake0084 <yuyake0084@gmail.com>
2019-12-30 13:35:29 +0900

    :construction: WIP

--- src/server/server.ts
+++ src/server/server.ts
@@ -37,8 +37,29 @@ export default (): void => {
   if (process.env.NODE_ENV !== 'test') {
     const server = createServer(app)
 
+    console.log('PORT!!!!!!!!!!!!!!!!!!!', port)
+
     server.listen(port, () => {
       console.log(`Listening on ${port}!🎉`)
+    })
+
+    server.on('error', (err: NodeJS.ErrnoException) => {
+      if (err.syscall !== 'listen') throw err
+
+      const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
+
+      switch (err.code) {
+        case 'EACCES':
+          console.error(`${bind} requires elevated privileges`)
+          process.exit(1)
+          break
+        case 'EADDRINUSE':
+          console.error(`${bind} is already in use`)
+          process.exit(1)
+          break
+        default:
+          throw err
+      }
     })
   }
 }
